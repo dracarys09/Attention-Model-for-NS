f936085bfad7f5dbee18eea851aeed1171648722
Abhijeet Dubey <dubey.abhijeet09@gmail.com>
2018-04-29 19:44:22 +0530

    config.py: Add required variables

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-DATA_PATH='/home/development/abhijeetd/NS/data/data.tsv'
-GLOVE_DIR_PATH='/home/development/abhijeetd/glove.twitter.27B'
-CHECKPOINT_FILE_PATH='/home/development/abhijeetd/NS/models/weights.best.hdf5'
+DATA_PATH='./data/data.tsv'
+GLOVE_DIR_PATH='./glove.twitter.27B'
+CHECKPOINT_FILE_PATH='./models/weights.best.hdf5'
 
 MAX_SENT_LENGTH=100
 MAX_SENTS=15
@@ -8,4 +8,4 @@ MAX_NB_WORDS=20000
 EMBEDDING_DIM=100
 VALIDATION_SPLIT=0.2
 NUM_EPOCHS=12
-BATCH_SIZE=50
+BATCH_SIZE=64
